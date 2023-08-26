7d8877d4ca0dae26628e216ee8eae1c1ce354a6b
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-08-25 22:19:19 -0400

    Update game with the game loop

--- coffee.py
+++ coffee.py
@@ -1,8 +1,6 @@
 """Making a coffee shop game in the terminal"""
-# Day Counter
-day = 1
-# Empty sales list to add dictionary info into
-sales = []
+# Import item to produce random numbers for the temperature
+from random import randint
 # Need a list of days that include
     # pricing, advertisement budget, sales, weather
 # sales = [
@@ -37,10 +35,41 @@ name = input("What is your name? ")
 shop_name = input(f"What is the name of {name}'s coffee shop? ")
 
 print(f"\nThanks, {name}. Let's set some initial pricing.\n")
-# Get initial price of a cup of coffee
-cup_price = input("What do you want to charger per cup of coffee? ")
-
 # Display what we have
 print("\nGreat. Here's what we've collected so far.\n")
 print(f"Your name is {name} and your opening {shop_name}!")
-print(f"Your first cup of coffee will sell for ${cup_price}.\n")
+# Start game
+print("\nOk, let's get started. Have fun!")
+# Day Counter
+day = 1
+# Starting cash
+cash = 100
+# Amount of cups of coffee on hand
+coffee = 100
+# Empty sales list to add dictionary info into
+sales = []
+
+running = True
+while running:
+    # Display the day and add a "fancy" text effect
+    print(f"\n-----| Day {day} @ {shop_name} |-----")
+    # Generate a random temp between 20 and 90
+    temperature = randint(20, 90)
+    # Display cash and weather
+    print(f"You have ${cash} cash and it's {temperature} degrees.")
+    print(f"You have coffee on hand to make {coffee} cups.\n")
+    # Get the price for the coffee
+    cup_price = input("What do you want to charger per cup of coffee? ")
+    print(f"Your first cup of coffee will sell for ${cup_price}.\n")
+    print("\nYou can buy advertising to help promote sales")
+    # Input on how much user wants to spend on advertisement
+    advertise = float(input("How much would you like to spend on advertisement today (0 is none)? "))
+    # Deduct advertising from cash on hand
+    cash -= advertise
+    # TODO: Calculate today's performance
+    # TODO: Display today's performance
+
+    # Before we loop around add a day
+    day += 1
+    if day == 2:
+        break
