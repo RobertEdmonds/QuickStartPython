494a043b8efc351444a301403df7e81df1401a89
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-09-06 14:35:33 -0400

    Finish chapter 14

---
+++ Part4/Chapter15/hello_flask.py
@@ -0,0 +1,11 @@
+"""This is for the flask example"""
+# import Flask
+from flask import Flask
+
+# Create the app object
+app = Flask(__name__)
+
+# Define the hello function, with route decorator
+@app.route("/")
+def hello():
+    return "<h1>Hello, World!</h1>"
